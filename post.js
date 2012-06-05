4257c4984f634f869df01197f97c33a97c12ee47
Eric Wieser <wieser.eric@gmail.com>
2012-06-05 18:14:40 +0200

    Replaced mixture of tabs and spaces with tabs

--- post.js
+++ post.js
@@ -1,6 +1,6 @@
 var el = document.getElementById('ctl00_cphRoblox_Createeditpost1_PostForm_PostBody'); /* Look for the default post body element */
-if(!el){ /* If it isn't found, then perhaps they use an extension that renames the element? */
-el = document.getElementById('wmd-input'); /* This should cover the Forum Enchancer */
+if(!el) { /* If it isn't found, then perhaps they use an extension that renames the element? */
+	el = document.getElementById('wmd-input'); /* This should cover the Forum Enchancer */
 }
 if (el) { /* If for some reason we still didn't find it, we're kinda screwed */
 	chrome.extension.sendRequest({ 
@@ -8,18 +8,19 @@ if (el) { /* If for some reason we still didn't find it, we're kinda screwed */
 	}, function(response) {
 		var sig = response.status;
 		if (el.value.search(sig) == -1) { 
-/* This is supposed to check if you posted and hit the floodcheck and went back to prevent putting the siggy in again */
+			/* This is supposed to check if you posted and hit the floodcheck and went back to prevent putting the siggy in again */
 			el.value = el.value + "\n\n"+sig.replace("\\n",String.fromCharCode(13)); /* Add the siggy to the box */
 		}
 
-        if (el.setSelectionRange) /* This little part moves the selection to the beginning of the postbody so you can start typing */
-            el.setSelectionRange(0, 0);
-        else if (el.createTextRange) {
-            var range = el.createTextRange();
-            range.collapse(true);
-            range.moveStart('character', 0);
-            range.moveEnd('character', 0);
-            range.select();
-        }
+		if (el.setSelectionRange) {
+			/* This little part moves the selection to the beginning of the postbody so you can start typing */
+			el.setSelectionRange(0, 0);
+		} else if (el.createTextRange) {
+			var range = el.createTextRange();
+			range.collapse(true);
+			range.moveStart('character', 0);
+			range.moveEnd('character', 0);
+			range.select();
+		}
 	});
 }
